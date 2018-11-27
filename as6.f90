c1dfa41fdab7d734230b6b4a324a1276a0df7571
connsteele <35544211+connsteele@users.noreply.github.com>
2018-11-27 15:03:06 -0800

    Add files via upload

--- as6.f90
+++ as6.f90
@@ -11,6 +11,7 @@ END TYPE myExprc
 
 ! Declare the array size for the input
 INTEGER :: arrSize = 3
+REAL :: returnVal !Interp output value
 TYPE(myExprc),DIMENSION(:),allocatable :: inputArray
 
 INTEGER :: i
@@ -18,18 +19,30 @@ allocate(inputArray(arrSize))
 inputArray(1)%char = '+'
 inputArray(1)%typeKey = 2
 inputArray(2)%num = 3
-inputArray(1)%typeKey = 1
+inputArray(2)%typeKey = 1
 inputArray(3)%num = 1
-inputArray(1)%typeKey = 1
+inputArray(3)%typeKey = 1
 
-print *, inputArray(1)%char
-
-! Loop that reads input array
-
-do i = 0, arrSize
-    if (i == 0 .AND. inputArray(i)%typeKey == 2) then
-        !check for operators here
-    end if 
+! Interpreter: Turn myExprc type into myValue type
+do i = 1, arrSize
+    ! Check for Arithmatic Operators
+    if (i == 1 .AND. inputArray(i)%typeKey == 2 ) then
+        ! Check for +
+        if (inputArray(1)%char == '+' .AND. arrSize >= 3) then
+            returnVal = inputArray(2)%num + inputArray(3)%num
+        else if (inputArray(1)%char == '-' .AND. arrSize >= 3) then
+            returnVal = inputArray(2)%num - inputArray(3)%num
+        else if (inputArray(1)%char == '*' .AND. arrSize >= 3) then
+            returnVal = inputArray(2)%num * inputArray(3)%num
+        else if (inputArray(1)%char == '/' .AND. arrSize >= 3 .AND. inputArray(3)%num .NE. 0) then
+            returnVal = inputArray(2)%num / inputArray(3)%num
+        else
+            print *, "Error: No Arithmatic Operator in Input"
+        endif
+        print *, returnVal ! Print out the return value, change this so it returns instead
+    else
+        ! Throw an error here, add else if statements to look for other things
+    end if
 end do
 
 deallocate(inputArray)
