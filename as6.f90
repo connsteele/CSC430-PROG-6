57eae2e5a8ff275b060c9fed80ea9db19eedeedd
Luis <verasteguil@yahoo.com>
2018-11-27 15:04:19 -0800

    some stuff done

--- as6.f90
+++ as6.f90
@@ -9,6 +9,7 @@ TYPE myExprc
     INTEGER :: typeKey !Would be 1 (for Reals), 2 (for Characters), and 3 (for Bools)
 END TYPE myExprc
 
+
 ! Declare the array size for the input
 INTEGER :: arrSize = 3
 TYPE(myExprc),DIMENSION(:),allocatable :: inputArray
@@ -34,6 +35,34 @@ end do
 
 deallocate(inputArray)
 
+CONTAINS
+	! String to Array function
+	function word (str)
+		CHARACTER(23) :: str
+		CHARACTER(10) :: word(5)
+		
+		INTEGER :: pos1 = 1, pos2, n = 0, i
+		DO
+			pos2 = INDEX(str(pos1:), ",")
+			IF (pos2 == 0) THEN
+			   n = n + 1
+			   word(n) = str(pos1:)
+			   EXIT
+			END IF
+			n = n + 1
+			word(n) = str(pos1:pos1+pos2-2)
+			pos1 = pos2+pos1
+		END DO
+		DO i = 1, n
+			WRITE(*,"(2A)", ADVANCE="NO") TRIM(word(i)), "."
+		END DO
+		RETURN 
+			
 
+	end function word
 
 end program
+
+
+
+
